e80c4e18d0673c842a50018b60c89583a065a309
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-13 11:15:13 -0700

    kola

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A c program that prints with put function
  *
- * Retutn: Always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 
