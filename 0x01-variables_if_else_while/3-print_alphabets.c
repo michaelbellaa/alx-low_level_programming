2b5461b088f510d96b507848eaa49b68e5ab2050
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-14 09:21:44 -0700

    kola

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
