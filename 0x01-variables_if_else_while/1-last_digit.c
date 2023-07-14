859ea3a8481150952b4133673fd8d30eae11136e
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-14 08:45:11 -0700

    kola

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,13 +3,12 @@
 #include <stdio.h>
 
 /**
- * main -entry point
+ *  main - Entry point
  *
- * Description: print the value of n status:
- *		greater than, is zero and is not less than 6g
+ * Description: A c program that prints if it greater or equal or less than 5
  *
- * Return : 0 (success)
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
