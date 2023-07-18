df715085d91a62df81ef7ed0004ca77c1627b0b1
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-18 04:05:32 -0700

    kola

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,17 @@
 
 /**
  * _is lower -  function to check if
- * 		character is lowercase
+ *		character is lowercase
  *
  * @c: checks input of function
  *
  * Return: returns 1 if c is lowercase
- * 	otherwise always 0 (suceess)
+ *	otherwise always 0 (suceess)
 */
 
 int _islower(int c)
 {
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
