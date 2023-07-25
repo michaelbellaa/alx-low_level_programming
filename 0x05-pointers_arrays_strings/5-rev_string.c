521ca36a0c44329f9d9edf3d5ba90ab8ef92c54d
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-25 07:52:53 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,11 +14,11 @@ void rev_string(char *s)
 	char temp;
 
 	/*find string length*/
-	for (l = 0; s[1] != '\0'; ++l)
+	for (l = 0; s[l] != '\0'; ++l)
 		;
 
 	/*swao the string*/
-	for (i = 0; i < 1 / 2; i++)
+	for (i = 0; i < l / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[l - 1 - i];
