3c82437a6f1895fd9564819e6d3a75afcb85b3ea
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-24 17:07:24 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints
+ * @s: pointer
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
