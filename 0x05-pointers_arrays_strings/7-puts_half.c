6818b20546f33c3197ca348fc3669a567039156a
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-24 17:39:42 -0700

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - kdcybudvcf
+ * @str: asbgcvyacdv
+ * Description: nvciuvhrub
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar('\n');
+	}
+}
