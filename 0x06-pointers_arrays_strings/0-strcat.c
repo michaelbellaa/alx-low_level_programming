829a0d99e8328c9d235589b81d5be68f3d5b41b7
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-27 17:15:02 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _stract - sdvgvbsydhbvcsdvjk feihubgfbbvf
+ *	dsnjdvnju
+ *
+ * @dest: dsfciuvcbnvubnusd
+ * @src: kvsdnsdvnujsvbn
+ *
+ * Return: fdvnbjfsdnvfjsd
+*/
+
+char *_stract(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2] ; c2++)
+
+		dest[c++] = src[c2];
+
+	return (dest);
+}
