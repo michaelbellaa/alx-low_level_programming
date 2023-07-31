01b13b2d6848438058670098a2809298c2f3fd0d
michaelbellaa <michael.ayoub622@gmail.com>
2023-07-31 16:51:58 -0700

    a

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memcpy - sfdvhybd
+ * @dest: dvjhvbfjdh
+ * @src: fdsyivbfu
+ * @n: fsdhiyvshgyus
+ *
+ * Return: dvjfnbdhb
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int itr;
+
+	for (itr = 0; itr < n; itr++)
+	{
+		dest[itr] = src[itr];
+	}
+	return (dest);
+}
+
