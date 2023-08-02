0407602c19b83d75917d323fb7dd47fa55240380
michaelbellaa <michael.ayoub622@gmail.com>
2023-08-02 07:51:48 -0700

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - fscv fsg
+ * @s :fjdnvijvbnfid
+ * Return: fvnjfdv
+*/
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\0')
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
