d85b794e80d3bda14b2387adccab5181315b402c
michaelbellaa <michael.ayoub622@gmail.com>
2023-08-02 10:43:26 -0700

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,3 +13,19 @@ int _sqrt_recursion(int n)
 	return (square(n, 1));
 }
 
+/**
+ * square - vfsdnvju
+ * @val: vfsdnnv
+ * @n : d fc vfn
+ * Return: sdfhbhsdv
+*/
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
