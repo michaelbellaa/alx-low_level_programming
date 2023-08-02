06510ffc7a07206fc33a6b21ed488002cb265987
michaelbellaa <michael.ayoub622@gmail.com>
2023-08-02 10:36:52 -0700

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - vfdsuhvfsdihu
+ * @n: foidksjnfvcoidvfn
+ * @val: dbsdvhb
+ * Return: vbfsdhvyb
+*/
+
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
