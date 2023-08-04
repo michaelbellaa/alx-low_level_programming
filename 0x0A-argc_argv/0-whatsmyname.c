49279b850d7f7f1c88cd5170e543895610d9b0e6
michaelbellaa <michael.ayoub622@gmail.com>
2023-08-04 02:21:39 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - vcbiub
+ * @argc: vfnsjudnv
+ * @argv: fdhvsug
+ * Return: fvhbsduv
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
