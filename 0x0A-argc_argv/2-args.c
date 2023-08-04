6b716e2f922391ebc0c762c9cb16edc90a7e45ee
michaelbellaa <michael.ayoub622@gmail.com>
2023-08-04 07:26:31 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - sfdvcbfesdhi
+ * @argc: uiwhd
+ * @argv: sdfhiufh
+ *
+ * Return: sdb
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
