0b7ee167f4e00933f3b1ca8fe0cf5b0d1ad1c08a
michaelbellaa <michael.ayoub622@gmail.com>
2023-08-04 07:22:44 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number
+ * @argc: fvhnsdoi
+ * @argv: fdu
+ *
+ * Return: alwyas zero
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
