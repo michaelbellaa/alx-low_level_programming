f0bc9dcda56eddb6b2e6cd20c8cdb59b65100366
michaelbellaa <michael.ayoub622@gmail.com>
2023-08-07 05:36:56 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup -  weifbfgiu
+ * @str: svnfdi
+ * @i: dvmnsoinv
+ * @size: sdvni
+ * @m: sidnfiub
+ * Return: fjsdvnoiv
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0; size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
+
